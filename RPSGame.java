3233d2da158bf69d9f01b1ddeb1c349c3738a354
yllekjjirpvul98 <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-10-12 00:02:05 +0100

    Three turns
    When it is a draw, that turn doesn't count
    When player wins two turn, then they win the game and vice versa

--- RPSGame.java
+++ RPSGame.java
@@ -1,6 +1,8 @@
 import java.util.Scanner; 
 import java.util.Random;
 public class RPSGame{
+    int win = 0;
+    int turn = 0;
     public int GetUserInput(){
         System.out.println("Press 1 to choose rock; Press 2 to choose paper; Press 3 to choose scissor");
         Scanner scan = new Scanner(System.in);
@@ -13,7 +15,7 @@ public class RPSGame{
        return compchoice;
     }
     public String DigitToChoice(int choice){
-	String result = " ";
+       String result = " ";
        if (choice == 1) {
            result = "Rock";
         }else if (choice ==2) {
@@ -24,23 +26,39 @@ public class RPSGame{
 	return result;
     }
     public void Results(String user, String comp) {
+        turn += 1;
         if (user == comp) {
             System.out.println ("Draw!");
+	    turn -= 1;
         }else if (user == "Rock" && comp == "Scissor" || user == "Paper" && comp == "Rock"||user == "Scissor" && comp == "Paper"){
-            System.out.println ("You win the game!");
+            System.out.println ("You win one turn!");
+	    win += 1;
         }else{
-            System.out.println("You lose the game!");
+            System.out.println("You lose one turn!");
         }
-    }    
+    }   
+    public void checkWin() {
+	if (turn == 2 && win ==2 || turn == 3 && win==2) {
+		System.out.println("Congratulations! You win the game!");
+		System.exit(0);
+	}else if(turn == 3 && win == 0 || turn == 3 && win == 1) {
+		System.out.println("Oops! You lose the game :(");
+		System.exit(0);
+	}
+    }
+	
+		
     public static void main(String[] args) {
         System.out.println("Welcome to the rock, paper and scissor game!");
         RPSGame game = new RPSGame();
-        String user_choice = game.DigitToChoice(game.GetUserInput());
-        String comp_choice = game.DigitToChoice(game.ComputerChoice());
-	System.out.println("You choose a " + user_choice +"!");
-        System.out.println("The computer choose a " + comp_choice+"!");
-        game.Results(user_choice, comp_choice);
-	System.out.println(user_choice + " " + comp_choice);
+	while (true) {
+        	String user_choice = game.DigitToChoice(game.GetUserInput());
+       		String comp_choice = game.DigitToChoice(game.ComputerChoice());
+		System.out.println("You choose a " + user_choice +"!");
+        	System.out.println("The computer choose a " + comp_choice+"!");
+        	game.Results(user_choice, comp_choice);
+		game.checkWin();
+	}
     }
 }
               
