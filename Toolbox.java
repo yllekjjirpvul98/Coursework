65023c7370faa401313f204ceaeeab8c7398ff51
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-07 00:25:57 +0000

    Add files via upload
    these files are under the project Tamagochi

--- Toolbox.java
+++ Toolbox.java
@@ -1,57 +1,105 @@
-/* To use Toolbox.java, copy and paste this code into a file called Toolbox.java
- * and save it in the directory with the rest of your code.
- */
 import java.io.*;
 import java.lang.Integer;
 import java.util.Random;
-public class Toolbox
-{
-	public Integer getRandomInteger(Integer max)
+	
+	public class Toolbox
+	
 	{
-		Random rand = new Random();
-		int number;
-		number = rand.nextInt(max) + 1;
-		return new Integer(number);
-	}
-	public Integer readIntegerFromCmd()
-	{
-		System.out.println("Enter your number");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String number;
-		number = null;
-		try
+
+		public Integer getRandomInteger(Integer max)
+
 		{
-			number = br.readLine();
-		}
-		catch (IOException ioe)
-		{
-			System.err.println("There was an input error");
-		}
-		try
-		{
+
+			Random rand = new Random();
+
+			int number;
+
+			number = rand.nextInt(max) + 1;
+
 			return new Integer(number);
+
 		}
-		catch (NumberFormatException e)
+
+		public Integer readIntegerFromCmd()
+
 		{
-			System.err
-					.println("There is something wrong with the number you entered");
+
+			System.out.println("Enter your number");
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String number;
+
+			number = null;
+
+			try
+
+			{
+
+				number = br.readLine();
+
+			}
+
+			catch (IOException ioe)
+
+			{
+
+				System.err.println("There was an input error");
+
+			}
+
+			try
+
+			{
+
+				return new Integer(number);
+
+			}
+
+			catch (NumberFormatException e)
+
+			{
+
+				System.err
+
+						.println("There is something wrong with the number you entered");
+
+			}
+
+			return 0;
+
 		}
-		return 0;
-	}
-	public String readStringFromCmd()
-	{
-		System.out.println("Enter your String");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String string;
-		string = null;
-		try
+
+		public String readStringFromCmd()
+
 		{
-			string = br.readLine();
+
+			System.out.println("Enter your String");
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			String string;
+
+			string = null;
+
+			try
+
+			{
+
+				string = br.readLine();
+
+			}
+
+			catch (IOException ioe)
+
+			{
+
+				System.err.println("There was an input error");
+
+			}
+
+			return string;
+
 		}
-		catch (IOException ioe)
-		{
-			System.err.println("There was an input error");
-		}
-		return string;
-	}
+
 }
