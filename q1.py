36b61c1d7c620c7bb6ee8ef29145697c646bdb4a
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-10 13:24:04 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,5 +4,18 @@ import numpy as np
 
 # Space for question 1
 def findbestprofit(pricesarray, k):
-    # Edit this method for your answer to question 1.
-    return -1
+    maxProfitArray = []
+    for i in range (len(pricearray)):
+        for ii in range (len(pricearray)):
+            if (i != ii):
+                maxProfit = pricearray[i] - pricearray[ii]
+                maxProfitArray.append(maxProfit)
+             
+   profitA = max(maxProfitArray)
+   for i in range(len(maxProfitArray)):
+        if (maxProfitArray[i] == profitA):
+            maxProfitArray.pop(i)
+            break
+   profitB = max(maxProfitArray)
+
+return profitA+profitB
