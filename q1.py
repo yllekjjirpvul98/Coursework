164833bc5bf4e78d64a4f061ef381e524f588433
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-10 13:53:11 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,6 +1,8 @@
 # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
 
 import numpy as np
+import heapq
+import random
 
 # Space for question 1
 def findbestprofit(pricesarray, k):
@@ -8,14 +10,17 @@ def findbestprofit(pricesarray, k):
     for i in range (len(pricearray)):
         for ii in range (len(pricearray)):
             if (i != ii):
-                maxProfit = pricearray[i] - pricearray[ii]
+                maxProfit = pricearray[ii] - pricearray[i]
                 maxProfitArray.append(maxProfit)
              
-   profitA = max(maxProfitArray)
-   for i in range(len(maxProfitArray)):
-        if (maxProfitArray[i] == profitA):
-            maxProfitArray.pop(i)
-            break
-   profitB = max(maxProfitArray)
-
-return profitA+profitB
+maxProfit = 0
+random.shuffle(maxPriceArray)
+num = 0
+if (k%2 == 0):
+    num = k/2
+else:
+    num = (k-1)/2
+largestNum = heapq.nlargest(num, x)
+for num in largestNum:
+    maxProfit += num
+return maxProfit
