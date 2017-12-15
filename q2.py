d3f375afcb85eded62cf9b44ef09e9c3e62a7d0f
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-15 23:45:53 +0000

    Update q2.py

--- q2.py
+++ q2.py
@@ -5,7 +5,34 @@ import numpy as np
 
 def findDiversifiedPortfolio(profitA, profitB):
     # Edit this method for your answer to question 2.
-    return -1
+    np_profitA = np.array(profitA)
+    np_profitB = np.array(profitB)
+    sizeA = np_profitA.shape[0]
+    sizeB = np_profitB.shape[0]
+    sumA = np.empty()
+    sumB = np.empty()
+    for x in xrange(sizeA):
+        for y in xrange(sizeA):
+            if (x != y and y > x):
+                subset = np_profitA[x:y]
+                sumA.append(np.sum(subset))
+    for x in xrange (sizeB):
+        for y in xrange(sizeB):
+            if (x != y and y > x):
+                subset = np_profitB[x:y]
+                sumB.append(np.sum(subset))
+    sizeofsumA = sumA.shape[0]
+    sizeofsumB = sumB.shape[0]
+    diffArray = np.empty()
+    for a in xrange(sizeofsumA):
+        for b in xrange(sizeofsumB):
+            diff = sumA[a] - sumB[b]
+            if (diff < 0):
+                diff = -diff
+            diffArray.append(diff)
+    return np.amin(diffArray)
+                
+            
 
 
 
