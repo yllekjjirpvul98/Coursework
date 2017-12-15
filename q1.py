8efd39a9c2e0944fcd281e718ebf09e621f119ea
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-15 23:03:19 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -6,24 +6,26 @@ import random
 
 # Space for question 1
 def findbestprofit(pricesarray, k):
-    maxProfitArray = []
-    for i in range (len(pricearray)):
-        for ii in range (len(pricearray)):
+    np_maxArray = np.empty()
+    np_priceArray = np.array(pricesarray)
+    size = np_priceArray.shape[1]
+    for i in xrange(size):
+        for ii in xrange(size):
             if (i != ii):
-                maxProfit = pricearray[ii] - pricearray[i]
-                maxProfitArray.append(maxProfit)
-             
-maxProfit = 0
-random.shuffle(maxPriceArray)
-num = 0
-if (k%2 == 0):
-    num = k/2
-else:
-    num = (k-1)/2
-if (num > 0):
-    largestNum = heapq.nlargest(num, x)
-    for num in largestNum:
-        maxProfit += num
-else:
-    maxProfit = 0
-return maxProfit
+                maxProfit = np_priceArray[ii] - np_priceArray[i]
+                np_maxArray.append(maxProfit)           
+    num = 0
+    if (k%2 == 0):
+        num = k/2
+    else:
+        num = (k-1)/2
+    if (num > 0):
+        maxProfit = np.amax(np_maxArray)
+        totalMax = 0
+        totalMax += maxProfit
+        while (num != 0):
+            np.delete(np_maxArray, [maxProfit])
+            maxProfit = np.amax(np_maxArray)
+            totalMax += maxProfit
+            num = num - 1
+        
