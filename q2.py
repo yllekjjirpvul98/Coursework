4f17d3008e117f46537fe0b6a74cb42fb9ee3af0
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-19 04:34:02 +0000

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,7 +1,14 @@
 # ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
 
 import numpy as np
+import itertools
 
+def findsubsets(S,m):
+    subsetArray = list()
+    for i in xrange(m):
+        subset = list(itertools.combinations(S, i))
+        subsetArray.append(subset)
+    return subsetArray
 
 def findDiversifiedPortfolio(profitA, profitB):
     # Edit this method for your answer to question 2.
@@ -9,30 +16,48 @@ def findDiversifiedPortfolio(profitA, profitB):
     np_profitB = np.array(profitB)
     sizeA = np_profitA.shape[0]
     sizeB = np_profitB.shape[0]
-    sumA = np.empty()
-    sumB = np.empty()
-    for x in xrange(sizeA):
-        for y in xrange(sizeA):
-            if (x != y and y > x):
-                subset = np_profitA[x:y]
-                sumA.append(np.sum(subset))
-    for x in xrange (sizeB):
-        for y in xrange(sizeB):
-            if (x != y and y > x):
-                subset = np_profitB[x:y]
-                sumB.append(np.sum(subset))
-    sizeofsumA = sumA.shape[0]
-    sizeofsumB = sumB.shape[0]
-    diffArray = np.empty()
+    setA = findsubsets(profitA, sizeA+1)
+    setB = findsubsets(profitB, sizeB+1)
+    np_setA = np.array(setA)
+    np_setB = np.array(setB)
+    np_sumA = np.empty(sizeA)
+    np_sumB = np.empty(sizeB)
+    np.copyto(np_sumA, profitA)
+    np.copyto(np_sumB, profitB)
+    print(np_setB)
+    sumA = list()
+    sumB = list()
+    for i in xrange(len(setA)):
+        if (i>1):
+            lists = setA[i]
+            for x in xrange(len(lists)):
+                sum = 0
+                tuples = lists[x]
+                for y in xrange(len(tuples)):
+                    sum += tuples[y]
+                sumA.append(sum)
+    np_sumA2 = np.array(sumA)
+    np_sumA = np.hstack((np_sumA, np_sumA2))
+    for i in xrange(len(setB)):
+        if (i>1):
+            lists = setB[i]
+            for x in xrange(len(lists)):
+                sum = 0
+                tuples = lists[x]
+                for y in xrange(len(tuples)):
+                    sum += tuples[y]
+                sumB.append(sum)
+    np_sumB2 = np.array(sumB)
+    np_sumB = np.hstack((np_sumB, np_sumB2))
+    sizeofsumA = np_sumA.shape[0]
+    sizeofsumB = np_sumB.shape[0]
+    diffArray = list()
     for a in xrange(sizeofsumA):
         for b in xrange(sizeofsumB):
-            diff = sumA[a] - sumB[b]
+            diff = np_sumA[a] - np_sumB[b]
             if (diff < 0):
                 diff = -diff
             diffArray.append(diff)
-    return np.amin(diffArray)
-                
-            
-
-
-
+    np_diffArray = np.array(diffArray)
+    print(np_diffArray)
+    return np.amin(np_diffArray)
