42449a2d5def3bddbd30f444471a2b2e73109615
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-19 02:34:05 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,14 +4,16 @@ import numpy as np
 
 # Space for question 1
 def findbestprofit(pricesarray, k):
-    np_maxArray = np.empty()
     np_priceArray = np.array(pricesarray)
     size = np_priceArray.shape[0]
+    maxProfitArray = list()
     for i in xrange(size):
         for ii in xrange(size):
-            if (i != ii):
+            if (i != ii and ii > i):
                 maxProfit = np_priceArray[ii] - np_priceArray[i]
-                np_maxArray.append(maxProfit)           
+                maxProfitArray.append(maxProfit)
+    np_maxArray = np.array(maxProfitArray)
+    print(np_maxArray)
     num = 0
     if (k%2 == 0):
         num = k/2
@@ -21,11 +23,19 @@ def findbestprofit(pricesarray, k):
         maxProfit = np.amax(np_maxArray)
         totalMax = 0
         totalMax += maxProfit
+        num = num - 1
         while (num != 0):
-            np_itemToDelete = np.empty()
-            np_itemToDelete.append(maxProfit)
+            itemToDelete = list()
+            itemToDelete.append(maxProfit)
+            np_itemToDelete = np.array(itemToDelete)
             np.delete(np_maxArray, np_itemToDelete)
             maxProfit = np.amax(np_maxArray)
             totalMax += maxProfit
             num = num - 1
+    else:
+        maxProfit = np.amax(np_maxArray)
+        totalMax = 0
+        totalMax += maxProfit
+    return totalMax
         
+
