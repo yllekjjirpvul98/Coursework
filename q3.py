966fa96629267a21a44dc161b0180a224059b62a
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2017-12-19 07:27:31 +0000

    Update q3.py

--- q3.py
+++ q3.py
@@ -9,21 +9,29 @@ def findSafeNetwork(numServers, loggingDeviceLimits, connectionDetails):
     numOfDevices = np_limit.shape[0]
     np_connect = np.array(connectionDetails)
     sizeofConnect = np_connect.shape[0]
-    np_allConnect = np.empty()
+    allConnect = list()
     for i in xrange(sizeofConnect):
-        np_allConnect.append(np_collect[i][0:2])
-        np_allConnect.append(np_collect[i][1:3])
-    np.unique(np_allConnect, axis=0)
-    sizeofAllConnect = np_allConnect.shape[0]
+        allConnect.append(np_connect[i][0:2])
+        allConnect.append(np_connect[i][1:3])
+    np_allConnect = np.array(allConnect)
+    np_allConnect = np.unique(np_allConnect, axis=0)
+    hashmap = {}
+    for connections in np_allConnect:
+        x = connections[0]
+        y = connections[1]
+        sum = x+y
+        if sum not in hashmap:
+            hashmap[sum] = 1
+        else:
+            hashmap[sum] += 1
+    sizeofAllConnect = len(hashmap.keys())
     connected = 0
     for x in xrange(numOfDevices):
-        sizeofAllConnect -= numOfDevices[x]
-        connected += numOfDevices[x]
-        if (sizeOfAllConnect <= 0):
-            if (sizeOfAllConnect < 0):
-                connected = connected + sizeOfAllConnect
-        return connected
-    sizeofAllConnect = np_allConnect.shape[0]
-    allConnect = np.concatenate((np_allConnect[0], np_allConnect[1]))
-    
-  
+        sizeofAllConnect -= loggingDeviceLimits[x]
+        connected += loggingDeviceLimits[x]
+        if (sizeofAllConnect <= 0):
+            if (sizeofAllConnect < 0):
+                connected = connected + sizeofAllConnect
+        if (sizeofAllConnect == numServers):
+            connected = connected - 1
+    return connected
